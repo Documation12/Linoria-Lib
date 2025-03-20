7222ae38d03d7b7017a1c36592f840a8ff120be1
drip <147793689+Documation12@users.noreply.github.com>
2025-03-20 01:21:29 -0500

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -40,7 +40,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] = { 1, httpService:JSONDecode('{"FontColor":"BFBFBF","MainColor":"171717","AccentColor":"8E25C6","BackgroundColor":"101010","OutlineColor":"0B0B0B"}') },
+		['Default'] = { 1, httpService:JSONDecode('{"FontColor":"BFBFBF","MainColor":"0F0F0F","AccentColor":"d17bff","BackgroundColor":"101010","OutlineColor":"0B0B0B"}') },
 	}
 
 	function ThemeManager:SetLibrary(library)
